14f82a8b8ac3401874489bab0ea8bb31f74d846c
maria-d-ferreira <maria.d.ferreira@sapo.pt>
2022-05-31 10:10:14 +0100

    add button show songs

--- src/components/Album.tsx
+++ src/components/Album.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Songs from "./Songs";
 import { ISong } from "../interfaces/interfaces";
 
@@ -14,20 +14,30 @@ interface Props {
 const Album: React.FC<Props> = props => {
   const { title, cover, year, songs } = props;
 
+  const [showSongs, setShowSongs] = useState(false);
+
+  const toggleShow = () => {
+    setShowSongs(prevShowSongs => !prevShowSongs);
+  };
+
   return (
     <>
       <li className="album">
         <img src={process.env.PUBLIC_URL + cover} alt={title} />
         <h2>{title}</h2>
         <h4>{year}</h4>
-
-        <ul className="songs-ul">
-          {songs.map(song => (
-            <li className="songs-li">
-              <Songs key={song.title} title={song.title} />
-            </li>
-          ))}
-        </ul>
+        <button onClick={toggleShow}>
+          {!showSongs ? "Songs" : "Discograhy"}
+        </button>
+        {showSongs && (
+          <ul className="songs-ul">
+            {songs.map(song => (
+              <li className="songs-li">
+                <Songs key={song.title} title={song.title} />
+              </li>
+            ))}
+          </ul>
+        )}
       </li>
     </>
   );
